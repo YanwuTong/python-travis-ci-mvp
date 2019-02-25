338b3a311e6e23114350ce639a2598e937ee1994
Phill Conrad <pconrad@cs.ucsb.edu>
2019-02-25 11:26:18 -0800

    Create test_temp.py

---
+++ test_temp.py
@@ -0,0 +1,18 @@
+import pytest
+
+from temp import cToF, fToC
+
+def test_cToF_10():
+   assert cToF(10.0) == pytest.approx(50.0)
+   
+def test_cToF_20():
+   assert cToF(20.0) == pytest.approx(68.0)
+   
+def test_fToC_32():
+   assert cToF(32.0) == pytest.approx(0.0)
+   
+def test_fToC_212():
+   assert cToF(212.0) == pytest.approx(100.0)
+   
+def test_fToC_n40():
+   assert cToF(-40.0) == pytest.approx(-40.0)
