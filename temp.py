e856751bd58e28cebfc9e067ceefcdae2ade2b7e
Phill Conrad <pconrad@cs.ucsb.edu>
2019-02-25 11:45:56 -0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,8 @@
 
 
 def cToF(cTemp):
-   return "stub"
-   #return cTemp * 9.0/5.0 + 32.0
+   return cTemp * 9.0/5.0 + 32.0
 
 
 def fToC(fTemp):
-   return "stub"
-   #return (fTemp - 32.0) * 5.0/9.0
+   return (fTemp - 32.0) * 5.0/9.0
